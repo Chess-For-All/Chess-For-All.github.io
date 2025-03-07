9a7b96e4a160406535c4a6efbe7c6cb6d7275b70
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2025-03-07 13:50:51 +0400

    Create button1.js

---
+++ .github/workflows/interactive/button1.js
@@ -0,0 +1,14 @@
+var x = 0
+
+function click() {
+  for (infinity) {
+    if (onclick) {
+      x = x + 1;
+    } else { 
+      x = x + 0;
+    }
+  }
+}
+
+const element = document.getElementById('like')
+element.style.text = 'like'
