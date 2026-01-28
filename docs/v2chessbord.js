3eaebcc58f2ea6d621b55851f6de49f015edfa56
mihail0011 <mihail.alekseevich.ucheniy@gmail.com>
2026-01-28 10:57:52 +0400

    Update v2chessbord.js

--- docs/v2chessbord.js
+++ docs/v2chessbord.js
@@ -1,5 +1,16 @@
 class Field {
-  constructor() {
-    this.color
-  }
+  constructor(fcolor, coords) {
+    this.color = fcolor;
+    this.isUncheked = true;
+    this.onstayed = ".";
+    this.coords = coords;
+  };
+  staylisten(piece) {
+    this.onstayed = piece;
+  };
+  movelisten() {
+    this.onstayed = ".";
+  };
 }
+
+var board
